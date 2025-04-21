24f755d7efd8f58f6e2af42b75b195dae8083384
nur awanis <nurawanis03@gmail.com>
2025-04-21 16:09:36 +0800

    Initial commit

---
+++ src/main/java/Week_04/example/ThreadNew.java
@@ -0,0 +1,10 @@
+package Week_04.example;
+
+public class ThreadNew {
+
+    public static void main(String[] args) {
+            Thread t = new Thread();
+            System.out.println(t.getState());
+        }
+}
+
